afe520fdd72b6a9694800ce7df83dcc563d951d2
kgonett <yasmin.chrystina@icloud.com>
2024-06-04 17:29:16 -0300

    versao2

---
+++ calc.js
@@ -0,0 +1,19 @@
+function insert(num){
+    document.getElementById('display').innerHTML += num;
+}
+function clean() {
+    document.getElementById('display').innerHTML = "";
+}
+function back() {
+    let resultado = document.getElementById('display').innerHTML;
+    document.getElementById('display').innerHTML = resultado.substring(0,resultado.length -1);
+}
+function calcular() {
+    let resultado = document.getElementById('display').innerHTML;
+    if (resultado) {
+        document.getElementById('display').innerHTML = eval(resultado);
+    }
+    else {
+        clean();
+    }
+}
